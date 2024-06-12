d92c2c08d00b884e6340095323f290b0d16f7323
Igor Minayeu <igor.minayeu@gmail.com>
2024-06-12 22:02:34 +0300

    lesson  components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
+function hello() {
+    debugger
+    alert('Hello!')
+}
 
+// hello()
+
+function App() {
+    debugger
+    return (
+        <div>
+            This is APP component
+            <Rating/>
+            <Accordion/>
         </div>
     );
+}
+
+function Rating() {
+    debugger
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+const Star = () => {
+    return <div>star</div>
+}
+
+function Accordion() {
+    debugger
+    return <div>
+        <h3>Menu</h3>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
 }
 
 export default App;
